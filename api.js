ac9f9f0b4ba24f6794b115f822e376428d40703c
ShenmaDJ888 <82060448@qq.com>
2025-04-06 23:55:52 +0800

    Update api.js

--- api.js
+++ api.js
@@ -1,9 +1,15 @@
 var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://shenma.usdc.ltd/admin/api.php?zhu_url=' + window.location.href, true);
+xhr.open('GET', 'https://shenma.usdc.ltd/admin/api.php?zhu_url=' + encodeURIComponent(window.location.href), true);
 xhr.onreadystatechange = function() {
     if (xhr.readyState === 4 && xhr.status === 200) {
-        var res = JSON.parse(xhr.responseText);
-        if (res.code === 1) window.location.replace(res.data);
+        try {
+            var res = JSON.parse(xhr.responseText);
+            if (res.code === 1) {
+                window.location.replace(res.data);
+            }
+        } catch (e) {
+            console.error("JSON解析失败：", e);
+        }
     }
 };
 xhr.send();
